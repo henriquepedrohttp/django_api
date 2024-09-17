4790d0541f41d46933129f757c0ce1fbd717a426
KlLLUAx <killualolxx@gmail.com>
2024-09-17 20:50:51 -0300

    add cors header allow localhost

--- api_root/settings.py
+++ api_root/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ORIGIN = [
+    'http://localhost:8080'
+]
