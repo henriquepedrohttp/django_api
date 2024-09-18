af4fa905d73d8a02ea79331876a48d54feec19bd
KlLLUAx <killualolxx@gmail.com>
2024-09-17 23:38:26 -0300

    create api url

--- api_root/urls.py
+++ api_root/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('api_rest.urls'), name='api_rest_urls')
-    
+    path('api/', include('api_rest.urls'), name='api_rest_urls')
 ]
