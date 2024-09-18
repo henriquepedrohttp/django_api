725d66409aacbb92f34f7dc6d650638b908f43de
KlLLUAx <killualolxx@gmail.com>
2024-09-17 22:41:37 -0300

    create get users method in views

--- api_rest/views.py
+++ api_rest/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
 # Create your views here.
+@api_view(['GET'])
+def get_users(request):
+  
+  if request.method == 'GET':
+    
+    users = User.objects.all()
+    
+    serializer = UserSerializer(users,many=True)
+    
+    return Response(serializer.data)
+  
+  return Response(status=status.HTTP_400_BAD_REQUEST)
